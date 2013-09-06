bed285678e90a6d942c43ed33ac8ef42bca86a7e
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-09-06 20:21:30 +0200

    BSL Evaluation

---
+++ Beginning Student Language/Evaluation.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Evaluation) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Expressions are evaluated left-to-right and inside-out
+;(+ 2 (* 3 4) (- (+ 1 2) 3))
+;(+ 2 12 (- 3 3))
+;(+ 2 12 0)
+
+; In Video Quizzes
+; Write out a step-by-step evalutation for
+;(/ 12 (+ (- 7 4) 3))
+;(/ 12 (+ 3 3))
+;(/ 12 6)
+
+; Select all calls to primitive
+; 1 and 3
+
+; Select all operators
+; 2 and 5
+
+; Select all operands
+; 3, 4, 6 and 7
